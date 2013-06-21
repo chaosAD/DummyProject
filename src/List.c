f322ae13043fc0fd19ae36908d920a66c020eae6
kaosad <frasker@hotmail.com>
2013-06-21 12:11:30 +0800

    Add comments on main() in main.c and functions in List.c

--- src/List.c
+++ src/List.c
@@ -1,12 +1,21 @@
 #include <stdio.h>
 #include <malloc.h>
 #include "List.h"
+/**
 
+
+Dummbo
+
+
+**/
 void memAllocError() {
   printf("System Error: insufficient memory\n");
   exit(-1);
 }
 
+/**
+ * Create a list object
+ */
 List *createList() {
   List *list = malloc(sizeof(List));
   if(!list) memAllocError();
--- src/main.c
+++ src/main.c
@@ -4,6 +4,8 @@
 
 // Add some message here
 
+/**
+//
 int main()
 {
     List *l = createList();
